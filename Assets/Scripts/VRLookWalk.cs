67e97ed07fe4bd65ba920864657706f76c4da0bf
Pavel Durov <paveldurov@Pavels-MacBook-Pro.local>
2018-02-18 13:26:51 +0000

    ínitial commit

---
+++ Assets/Scripts/VRLookWalk.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour {
+	
+	public Transform vrCamera;
+
+	public float toggleAngleMin = 30.0f;
+
+	public float toggleAngleMax = 90.0f;
+
+	public float speed = 10.0f;
+
+	private CharacterController _characterController;
+
+	void Start () {
+		_characterController = GetComponent<CharacterController>();
+	}
+		
+	void Update () {
+		float xAngle = vrCamera.eulerAngles.x;
+
+		if (xAngle >= toggleAngleMin && xAngle < toggleAngleMax) {
+			Vector3 forward = vrCamera.TransformDirection (Vector3.forward);
+
+			var step = forward * speed;
+			_characterController.SimpleMove (step);
+		}
+	}
+}
+
