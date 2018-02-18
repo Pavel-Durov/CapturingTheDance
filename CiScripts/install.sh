239190fbe7ae2cd087cf9056b8af68da75c09a8f
Pavel Durov <paveldurov@Pavels-MacBook-Pro.local>
2018-02-18 15:21:36 +0000

    downloading unity package

--- CiScripts/install.sh
+++ CiScripts/install.sh
@@ -1,9 +1,7 @@
 #! /bin/sh
 
-# This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
-# installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from https://netstorage.unity3d.com/unity/fc1d3344e6ea/UnityDownloadAssistant-2017.3.1f1.dmg'
-curl -o Unity.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/UnityDownloadAssistant-2017.3.1f1.dmg
+echo "https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg?_ga=2.124654157.617447808.1518806906-2146109841.1516133558"
+curl -o Unity-2017.3.1f1.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg?_ga=2.124654157.617447808.1518806906-2146109841.1516133558
 
 echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+sudo installer -dumplog -package Unity-2017.3.1f1.pkg -target /
