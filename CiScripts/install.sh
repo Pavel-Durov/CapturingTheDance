b6d3bccea1cb90b885b1762c3e45f1ec7a02c4a1
Pavel Durov <paveldurov@Pavels-MacBook-Pro.local>
2018-02-18 15:41:38 +0000

    add exectution mode to install.sh

--- CiScripts/install.sh
+++ CiScripts/install.sh
@@ -4,4 +4,4 @@ echo "https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity
 curl -o Unity-2017.3.1f1.pkg https://netstorage.unity3d.com/unity/fc1d3344e6ea/MacEditorInstaller/Unity-2017.3.1f1.pkg?_ga=2.124654157.617447808.1518806906-2146109841.1516133558
 
 echo 'Installing Unity.pkg'
-sudo -E su $USER -c 'installer -dumplog -package Unity-2017.3.1f1.pkg -target /'
+sudo installer -dumplog -package Unity-2017.3.1f1.pkg -target '
