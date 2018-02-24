2ef576230e9292e8721b78aa55851cf8a752144d
Pavel Durov <paveldurov@Pavels-MacBook-Pro.local>
2018-02-24 18:37:59 +0000

    Avoid player steping out of scene borders

--- Assets/Scripts/VRLookWalk.cs
+++ Assets/Scripts/VRLookWalk.cs
@@ -2,16 +2,16 @@
 
 public class VRLookWalk : MonoBehaviour
 {
+	
+	public Transform VrCamera;
+    public Terrain TerrainBorder;
 
-    public Transform VrCamera;
+	public float ToggleAngleMin = 30.0f;
+	public float ToggleAngleMax = 90.0f;
+	public float Speed = 10.0f;
 
-    public float ToggleAngleMin = 30.0f;
 
-    public float ToggleAngleMax = 90.0f;
-
-    public float Speed = 10.0f;
-
-    private CharacterController _characterController;
+	private CharacterController _characterController;
     private float _rotationY = 0.0f;
     private float _rotationX = 0.0f;
 
@@ -21,56 +21,71 @@ public class VRLookWalk : MonoBehaviour
     private const float SENS_X = 100.0f;
     private const float SENS_Y = 100.0f;
 
+	void Start ()
+	{
+		_characterController = GetComponent<CharacterController> ();
+	}
 
-    void Start()
-    {
-        _characterController = GetComponent<CharacterController>();
+	private bool IsMoveForwardAngle (Vector3 angle)
+	{
+		return angle.x > ToggleAngleMin && angle.x < ToggleAngleMax;
+	}
+
+
+    private bool IsStepOutOfBorders(Vector3 step){
+        var position = TerrainBorder.GetPosition();
+        var size = TerrainBorder.terrainData.size;
+
+        var nextX = VrCamera.position.x + step.x;
+        var nextZ = VrCamera.position.z + step.z;
+
+        var isXOut = (nextX < position.x || nextX > position.x + size.x);
+        var isZOut = (nextZ < position.z || nextZ > position.z + size.z);
+
+        return isXOut || isZOut;
+
     }
+	private Vector3 NextForwardStep {
+		get {
+			Vector3 step = VrCamera.TransformDirection (Vector3.forward);
+			step *= Speed * Time.deltaTime;
 
-    private bool IsMoveForwardAngle(Vector3 angle)
-    {
-        return angle.x > ToggleAngleMin && angle.x < ToggleAngleMax;
-    }
+            if(IsStepOutOfBorders(step)){
+                //Avoid player to fall into the void if hes out of scene border
+                step = Vector3.zero;
+            }
 
-    private Vector3 NextForwardStep
-    {
-        get
-        {
-            Vector3 step = VrCamera.TransformDirection(Vector3.forward);
-            step *= Speed * Time.deltaTime;
-            step.y = 0;
-            return step;
-        }
+            if(step.y < 0){
+                step.y = 0;
+            }
+			return step;
+		}
 
-    }
+	}
 
-    void Update()
-    {
-#if UNITY_ANDROID || UNITY_IPHONE
+	void Update ()
+	{
+	    #if UNITY_ANDROID || UNITY_IPHONE
             MoveForwardByAngle();
-#else
-        RotateCamera();
-        MoveForwardByAngle();
-#endif
+		#else
+		    RotateCamera();
+            MoveForwardByAngle();
+		#endif
+		
+	}
 
-    }
+	void MoveForwardByAngle(){
+		if (IsMoveForwardAngle (VrCamera.eulerAngles)) {
+			_characterController.Move (NextForwardStep);
+		}
+	}
 
-    void MoveForwardByAngle()
-    {
-        if (IsMoveForwardAngle(VrCamera.eulerAngles))
-        {
-            _characterController.Move(NextForwardStep);
-        }
-    }
-
-    void RotateCamera()
-    {
-
+    void RotateCamera(){
         _rotationX += Input.GetAxis("Mouse X") * SENS_X * Time.deltaTime;
         _rotationY += Input.GetAxis("Mouse Y") * SENS_Y * Time.deltaTime;
         _rotationY = Mathf.Clamp(_rotationY, MIN_Y, MAX_Y);
 
         _characterController.transform.localEulerAngles = new Vector3(-_rotationY, _rotationX, 0);
-    }
+	}
 }
 
